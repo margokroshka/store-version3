78b4bdee68ec6711f6da9441566227396eb2d80f
MargaretBaranovskaya <95936987+margokroshka@users.noreply.github.com>
2024-06-03 14:55:16 +0300

    Add files via upload

---
+++ pet-product-service/src/test/java/com/example/petproductservice/PetProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
