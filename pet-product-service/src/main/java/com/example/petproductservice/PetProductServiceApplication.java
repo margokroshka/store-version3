40e6f4ab93fd77f51549cc1f41f9511e424dc4c3
MargaretBaranovskaya <95936987+margokroshka@users.noreply.github.com>
2024-06-17 11:28:15 +0300

    Revert "First pr"

---
+++ pet-product-service/src/main/java/com/example/petproductservice/PetProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.petproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProductServiceApplication.class, args);
+    }
+
+}
