8163ad7ed2904f023312f476d0cd39a65605ce65
MargaretBaranovskaya <95936987+margokroshka@users.noreply.github.com>
2024-06-17 15:08:40 +0300

    Revert "First pr"

---
+++ pet-inventory-service/src/main/java/com/example/petinventoryservice/PetInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.petinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetInventoryServiceApplication.class, args);
+    }
+
+}
